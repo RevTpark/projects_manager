31a7cc33f42e6a6479284ad96f5dbf9c6b7a30ae
Tanishq Parkar <78678620+RevTpark@users.noreply.github.com>
2022-05-23 21:32:36 +0530

    getting started with basics

---
+++ file_manager/src/file_iterator.rs
@@ -0,0 +1,78 @@
+use std::fs;
+use std::path::{Path, PathBuf};
+use std::ffi::OsStr;
+use regex::Regex;
+use std::collections::HashMap;
+
+use crate::constants::GlobalConstants;
+
+pub fn iterate_over_files() -> Vec<HashMap<String, String>>{
+    let base_directory: String = GlobalConstants::new().base_directory;
+    let mut final_projects_list: Vec<HashMap<String, String>> = vec![];
+    let project_directories: Vec<String> = GlobalConstants::new().project_directories;
+    for directory in project_directories{
+        for file in fs::read_dir(base_directory.clone()+&directory).unwrap(){
+            let mut langs: HashMap<String, u16> = HashMap::new();
+            let path: PathBuf = file.unwrap().path();
+            let string_path: String = path.display().to_string();
+            iterate_files(string_path, &mut langs);
+            let mut max_name: String = String::new(); 
+            let mut max_points: u16 = 0;
+            for (language, points) in &langs{
+                if *points > max_points{
+                    max_points = *points;
+                    max_name = language.clone();
+                }
+            }
+            let new_project: HashMap<String, String> = HashMap::from([
+                (path.display().to_string(), max_name)
+            ]);
+            final_projects_list.push(new_project);
+        }
+    }
+    return final_projects_list;
+}
+
+fn is_safe_to_iterate(filename: &std::ffi::OsStr) -> bool{
+    let restricted: Vec<&str> = vec!["andriod", "build", "ios", "target", "ckeditor", "material-dashboard", "node_modules", "venv", "__pycache__", "migrations"];
+    let reg: Regex = Regex::new(r"^\.").unwrap();
+    reg.is_match(&filename.to_str().unwrap()) || restricted.contains(&filename.to_str().unwrap())
+}
+
+fn iterate_files(path: String, langs: &mut HashMap<String, u16>){
+    let files = fs::read_dir(&path);
+    match &files{
+        Ok(_) => {
+            for file in files.unwrap(){
+                let path: PathBuf = file.unwrap().path();
+                let string_path: String = path.display().to_string();
+                let temp = Path::new(&string_path).file_name().unwrap();
+                if is_safe_to_iterate(temp){
+                    continue;
+                }
+                
+                match Path::new(&path).extension().and_then(OsStr::to_str){
+                    None => {
+                        iterate_files(string_path, langs);
+                        continue;
+                    },
+                    Some(text) => {
+                        let ext: &str = text;
+                        let allowed_exts: Vec<&str> = vec!["png", "jpeg", "jpg", "class", "json", "exe", "pdf"];
+                        if !allowed_exts.contains(&ext){
+                            if langs.contains_key(ext){
+                                let counter = langs.entry(String::from(ext)).or_insert(0);
+                                *counter += 1;
+                            }
+                            else{
+                                langs.insert(String::from(ext), 1);
+                            }
+                        }
+                        
+                    }
+                }
+            }
+        },
+        Err(_) => {},
+    }
+}
--- file_manager/src/lib.rs
+++ file_manager/src/lib.rs
@@ -1,5 +1,11 @@
-
+// Python Module
 use pyo3::prelude::*;
+
+// Custom
+mod constants;
+mod file_iterator;
+use file_iterator::iterate_over_files;
+use std::collections::HashMap;
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
@@ -7,16 +13,15 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
-
 #[pyfunction]
-fn get_all_projects() -> PyResult<String> {
-    
-    Ok(String::new())
+fn get_all_projects() -> PyResult<Vec<HashMap<String, String>>> {
+    Ok(iterate_over_files())
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn file_manager(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(get_all_projects, m)?)?;
     Ok(())
 }
